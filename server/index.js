650b8b929bf24875c6fad3aaba7d07a5d4d05612
amar ali <amarhussain391@gmail.com>
2024-08-06 18:37:42 +0500

    commited

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 const corspolicy = {
-  origin: "*",
+  origin: "https://e-task-lac.vercel.app",
   Credential: true,
   method: ["GET", "POST", "PUT", "DELETE"],
 };
